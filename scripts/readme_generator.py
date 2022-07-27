2b53c909d2a4286b7ff5501cf6de2521cc09829f
My <34188127+0xcaffebabe@users.noreply.github.com>
2022-07-27 07:16:21 +0000

    🛠️脚本

---
+++ scripts/readme_generator.py
@@ -0,0 +1,49 @@
+import os
+from urllib.parse import quote
+
+def walk(path, kw, func) -> list:
+
+    g = os.walk(path)
+    result = []
+    for path, dir_list, file_list in g:
+        for dir_name in dir_list:
+            if not '.' in dir_name:
+                continue
+            dir = os.path.join(path, dir_name)
+            if kw in dir:
+                result.append(dir)
+
+    result.sort(key=func)
+    return result
+
+normal_result = walk("./算法", "00", lambda x: x)
+db_result = walk("./数据库", "", lambda x: x)
+normal_result += db_result
+normal_result.sort(key = lambda x: int(x.split("/")[-1].split(".")[0]))
+
+offer_result = walk("./算法/剑指Offer", "剑指", lambda x: x)
+interview_result = walk("./算法/面试题", "面试题", lambda x: x)
+lcp_result = walk("./算法/LCP", "LCP", lambda x: x)
+
+all_result = normal_result + interview_result + lcp_result
+
+md_content = """
+序号 | 题目
+-|-
+"""
+
+for i in all_result:
+  if os.path.exists(i + "/README.md"):
+    name = i.split("/")[-1]
+    seq = name.split(".")[0]
+    md_content += "{} | [{}](https://github.com/0xcaffebabe/leetcode/tree/main{})".format(seq, name, quote(i[1:])) + "\n"
+
+f = open('./scripts/template.md', 'r', encoding="utf8")
+template = f.read()
+f.close()
+
+f = open('README.md', 'w', encoding="utf8")
+f.write(template.replace("{{solutions}}", md_content))
+f.close()
+
+print("done")
---
+++ scripts/template.md
@@ -0,0 +1,5 @@
+# leetcode
+
+[![wakatime](https://wakatime.com/badge/user/78591c59-95d5-4479-b2fc-988c35f31d59/project/dba7be3d-deae-42c4-b99f-3b362eae1a0b.svg)](https://wakatime.com/badge/user/78591c59-95d5-4479-b2fc-988c35f31d59/project/dba7be3d-deae-42c4-b99f-3b362eae1a0b) [![leetcode](https://leetcode-badge.haozibi.dev/v1cn/solved/0xcaffebabe.svg)](https://leetcode.cn/u/0xcaffebabe/) [![leetcode](https://leetcode-badge.haozibi.dev/v1cn/accepted-rate/0xcaffebabe.svg)](https://leetcode.cn/u/0xcaffebabe/)
+
+{{solutions}}
