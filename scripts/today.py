5d2c2c034654f934f985c9f9867ea2e5edb1aaf1
My <34188127+0xcaffebabe@users.noreply.github.com>
2022-08-12 03:43:47 +0000

    🛠️每日一题爬取

---
+++ scripts/today.py
@@ -0,0 +1,23 @@
+import requests
+session = requests.session()
+req_body = {"query":"\n    query questionOfToday {\n  todayRecord {\n    date\n    userStatus\n    question {\n      questionId\n      frontendQuestionId: questionFrontendId\n      difficulty\n      title\n      titleCn: translatedTitle\n      titleSlug\n      paidOnly: isPaidOnly\n      freqBar\n      isFavor\n      acRate\n      status\n      solutionNum\n      hasVideoSolution\n      topicTags {\n        name\n        nameTranslated: translatedName\n        id\n      }\n      extra {\n        topCompanyTags {\n          imgUrl\n          slug\n          numSubscribed\n        }\n      }\n    }\n    lastSubmission {\n      id\n    }\n  }\n}\n    ","variables":{}}
+
+headers = {
+  'Content-Type': 'application/json'
+}
+resp = requests.post("https://leetcode.cn/graphql/", headers=headers, json=req_body)
+data = resp.json()['data']['todayRecord'][0]
+
+date = data['date']
+id = data['question']['frontendQuestionId']
+difficulty = data['question']['difficulty']
+title = data['question']['titleCn']
+ac_rate = data['question']['acRate']
+
+print("""
+    日期: {}
+    题目: {}. {}
+    难度: {}
+    通过率: {}
+""".format(date, id, title, difficulty, round(ac_rate * 100, 2)))
+
