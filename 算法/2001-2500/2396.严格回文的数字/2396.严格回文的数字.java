d527f97f846d731e94194b7c064abf72d7721cc7
MY <715711877@qq.com>
2023-01-22 20:25:45 +0800

    ➕2396

---
+++ 算法/2001-2500/2396.严格回文的数字/2396.严格回文的数字.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=2396 lang=java
+ *
+ * [2396] 严格回文的数字
+ * 
+ * 67/67 cases passed (0 ms)
+ * Your runtime beats 100 % of java submissions
+ * Your memory usage beats 78.34 % of java submissions (38.2 MB)
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
+// @lc code=end
+
