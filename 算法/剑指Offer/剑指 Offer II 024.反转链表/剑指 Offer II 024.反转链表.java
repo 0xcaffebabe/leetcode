e884d0e9e8a0e7dfab65a386bb42a6b08e2c4d19
MY <715711877@qq.com>
2023-05-22 14:50:41 +0800

    ➕024

---
+++ 算法/剑指Offer/剑指 Offer II 024.反转链表/剑指 Offer II 024.反转链表.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=剑指 Offer II 024 lang=java
+ * @lcpr version=21908
+ *
+ * [剑指 Offer II 024] 反转链表
+ * 
+ * 28/28 cases passed (1 ms)
+ * Your runtime beats 3.97 % of java submissions
+ * Your memory usage beats 85.09 % of java submissions (40.6 MB)
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        LinkedList<Integer> stack = new LinkedList<>();
+        while(head != null){
+            stack.push(head.val);
+            head = head.next;
+        }
+        ListNode root = new ListNode();
+        ListNode origin = root;
+        while(!stack.isEmpty()){
+            root.val = stack.pop();
+            if (!stack.isEmpty()){
+                root.next = new ListNode();
+                root = root.next;
+            }
+        }
+        return origin;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
