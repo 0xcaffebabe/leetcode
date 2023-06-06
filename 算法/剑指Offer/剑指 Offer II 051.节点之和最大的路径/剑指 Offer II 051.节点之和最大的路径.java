609d2b155248d4316e250b91a64f76852280703f
MY <715711877@qq.com>
2023-06-06 15:30:19 +0800

    ➕051

---
+++ 算法/剑指Offer/剑指 Offer II 051.节点之和最大的路径/剑指 Offer II 051.节点之和最大的路径.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=剑指 Offer II 051 lang=java
+ * @lcpr version=21908
+ *
+ * [剑指 Offer II 051] 节点之和最大的路径
+ * 
+ * 94/94 cases passed (0 ms)
+ * Your runtime beats 100 % of java submissions
+ * Your memory usage beats 92.62 % of java submissions (42.5 MB)
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+    int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = Math.max(dfs(root.left), 0);
+        int right = Math.max(dfs(root.right), 0);
+        max = Math.max(root.val + left + right, max);
+        return root.val + Math.max(left, right);
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-10,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+ */
+
