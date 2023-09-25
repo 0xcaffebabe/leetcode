28e3eb9b097dd885acef17b977ab3a309578ba8d
MY <715711877@qq.com>
2023-09-25 17:10:29 +0800

    ➕2769

---
+++ 算法/2501-3000/2769.找出最大的可达成数字/2769.找出最大的可达成数字.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=2769 lang=java
+ * @lcpr version=
+ *
+ * [2769] 找出最大的可达成数字
+ * 
+ * 2502/2502 cases passed (1 ms)
+ * Your runtime beats 100 % of java submissions
+Your memory usage beats 49.51 % of java submissions (39.2 MB)
+ */
+
+// @lc code=start
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 4\n1\n
+// 5\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n2\n
+// 50\n50\n
+// 1\n1\n
+// @lcpr case=end
+
+ */
+
