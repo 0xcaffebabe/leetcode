88b07edca22958b7a533c75fb5328a3964815f75
MY <715711877@qq.com>
2023-10-10 09:24:41 +0800

    ➕877

---
+++ 算法/501-1000/877.石子游戏/877.石子游戏.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=877 lang=java
+ * @lcpr version=
+ *
+ * [877] 石子游戏
+ * 
+ * 46/46 cases passed (0 ms)
+ * Your runtime beats 100 % of java submissions
+ * Your memory usage beats 85.19 % of java submissions (39 MB)
+ */
+
+// @lc code=start
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [5,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,7,2,3]\n
+// @lcpr case=end
+
+ */
+
